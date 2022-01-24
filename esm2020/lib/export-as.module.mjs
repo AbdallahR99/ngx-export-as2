a5b4512ee506b973e80dafc23db0897446821231
Rowyda AlRashedy <rowyda_alrashedy@outlook.com>
2022-01-24 20:21:48 +0400

    ngx-export-as-fixed

---
+++ esm2020/lib/export-as.module.mjs
@@ -0,0 +1,21 @@
+/**
+ * angular imports
+ */
+import { NgModule } from '@angular/core';
+/**
+ * my imports
+ */
+import { ExportAsService } from './export-as.service';
+import * as i0 from "@angular/core";
+export class ExportAsModule {
+}
+ExportAsModule.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "13.0.3", ngImport: i0, type: ExportAsModule, deps: [], target: i0.ɵɵFactoryTarget.NgModule });
+ExportAsModule.ɵmod = i0.ɵɵngDeclareNgModule({ minVersion: "12.0.0", version: "13.0.3", ngImport: i0, type: ExportAsModule });
+ExportAsModule.ɵinj = i0.ɵɵngDeclareInjector({ minVersion: "12.0.0", version: "13.0.3", ngImport: i0, type: ExportAsModule, providers: [ExportAsService] });
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "13.0.3", ngImport: i0, type: ExportAsModule, decorators: [{
+            type: NgModule,
+            args: [{
+                    providers: [ExportAsService],
+                }]
+        }] });
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiZXhwb3J0LWFzLm1vZHVsZS5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uLy4uLy4uLy4uL3Byb2plY3RzL25neC1leHBvcnQtYXMvc3JjL2xpYi9leHBvcnQtYXMubW9kdWxlLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJBQUFBOztHQUVHO0FBQ0gsT0FBTyxFQUFFLFFBQVEsRUFBRSxNQUFNLGVBQWUsQ0FBQztBQUV6Qzs7R0FFRztBQUNILE9BQU8sRUFBRSxlQUFlLEVBQUUsTUFBTSxxQkFBcUIsQ0FBQzs7QUFLdEQsTUFBTSxPQUFPLGNBQWM7OzJHQUFkLGNBQWM7NEdBQWQsY0FBYzs0R0FBZCxjQUFjLGFBRmQsQ0FBQyxlQUFlLENBQUM7MkZBRWpCLGNBQWM7a0JBSDFCLFFBQVE7bUJBQUM7b0JBQ1IsU0FBUyxFQUFFLENBQUMsZUFBZSxDQUFDO2lCQUM3QiIsInNvdXJjZXNDb250ZW50IjpbIi8qKlxyXG4gKiBhbmd1bGFyIGltcG9ydHNcclxuICovXHJcbmltcG9ydCB7IE5nTW9kdWxlIH0gZnJvbSAnQGFuZ3VsYXIvY29yZSc7XHJcblxyXG4vKipcclxuICogbXkgaW1wb3J0c1xyXG4gKi9cclxuaW1wb3J0IHsgRXhwb3J0QXNTZXJ2aWNlIH0gZnJvbSAnLi9leHBvcnQtYXMuc2VydmljZSc7XHJcblxyXG5ATmdNb2R1bGUoe1xyXG4gIHByb3ZpZGVyczogW0V4cG9ydEFzU2VydmljZV0sXHJcbn0pXHJcbmV4cG9ydCBjbGFzcyBFeHBvcnRBc01vZHVsZSB7IH1cclxuXHJcblxyXG4iXX0=
